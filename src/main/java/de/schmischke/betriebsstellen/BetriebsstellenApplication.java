ff43966e50d846788545292fe3abf7a95ffd35af
Leonard Schmischke <LS@roconcept.de>
2021-06-12 21:35:41 +0200

    init

---
+++ src/main/java/de/schmischke/betriebsstellen/BetriebsstellenApplication.java
@@ -0,0 +1,13 @@
+package de.schmischke.betriebsstellen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetriebsstellenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetriebsstellenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/schmischke/betriebsstellen/BetriebsstellenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schmischke.betriebsstellen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetriebsstellenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
